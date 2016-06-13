051de7c4a7f1a09b78e7095912f272166a919fab
kellerd <daniel.allan.keller@gmail.com>
2016-06-12 20:33:51 -0400

    Data types

--- GreyTide.fsx
+++ GreyTide.fsx
@@ -5,11 +5,36 @@ open FSharp.Data
 open XPlot.GoogleCharts
 open XPlot.GoogleCharts.Deedle
 
-type States = FSharp.Data.JsonProvider<"""http://greytide.azurewebsites.net/tide/States""">
-type Models = FSharp.Data.JsonProvider<"""http://greytide.azurewebsites.net/tide/Models""">
-let states = States.Load("""http://greytide.azurewebsites.net/tide/States""")
-let models = Models.Load("""http://greytide.azurewebsites.net/tide/Models""")
+type States = JsonProvider<"""http://greytide.azurewebsites.net/tide/v1/States""">
+type Models = JsonProvider<"""http://greytide.azurewebsites.net/tide/v1/Models/""">
+Models.GetSamples().[0]
+let states = States.Load("""http://greytide.azurewebsites.net/tide/States/v1/Models/""")
+let models = Models.Load("""http://greytide.azurewebsites.net/tide/Models/v1/Models/""")
+//State.Events.StateCollectionId changed
 
+let mapStates = states |> Array.map (fun s -> s.Id, 
+                                              s.Id2,
+                                              s.Name,
+                                              s.Type,
+                                              s.Events 
+                                              |> Array.map (fun (e :States.Event) -> 
+                                                                     e.Name, 
+                                                                     e.Id, 
+                                                                     e.To, 
+                                                                     e.Order, 
+                                                                     e.StateCollectionId, 
+                                                                     e.From
+                                                                     |> Array.map (fun (f : States.From) -> 
+                                                                                            f.Name,
+                                                                                            f.Type,
+                                                                                            f.Id,
+                                                                                            f.StateId)))
+
+type Person = JsonProvider<"""[{"name":"Dan", "language":"F#"}]""">
+ //                           ,{"name":"Dad"}
+let samples = Person.GetSamples()
+
+samples |> Array.map (fun p -> p.Name.Length + p.Language.Length)
 
 let rawData = models 
                 |> Array.collect (fun m -> m.States 
@@ -20,6 +45,28 @@ let eventMap =
     |> Array.collect (fun m -> m.Events) 
     |> Array.map(fun e -> e.Name, e.To)
     |> Map.ofArray
+
+models 
+|> Array.groupBy (fun model -> model.Faction,model.CurrentState) 
+|> Array.map(fun ((faction,state),models) -> faction,state,models) 
+|> Frame.ofValues
+|> Frame.map (fun f s ms -> ms |> Array.sumBy (fun (m:Models.Root) -> m.Points))
+|> Frame.fillMissingWith 0
+|> Chart.Bar 
+|> Chart.WithLegend true 
+
+
+
+//
+
+//bug in newer XPLot.Deedle, could do , has to update some references
+// Workaround with convert frame to series
+// let rows : Series<string,Series<string,int>> = data |> Frame.getRows
+// rows |> Series.values |> Chart.Bar |> Chart.WithLabels rows.Keys
+
+
+
+//Other versions of getting Data
 
 // let defaultData = eventMap |> Map.toArray 
 //                     |> Array.map (fun (_,e) -> 0, e) 
@@ -50,21 +97,3 @@ let eventMap =
 //                                     |> Series.groupBy (fun key (state,points) -> state)
 //                                     |> Series.map (fun key series -> series |> Series.values |> Seq.sumBy snd))
 
-
-models 
-|> Array.groupBy (fun model -> model.Faction,model.CurrentState) 
-|> Array.map(fun ((faction,state),models) -> faction,state,models) 
-|> Frame.ofValues
-|> Frame.map (fun f s ms -> ms |> Array.sumBy (fun (m:Models.Root) -> m.Points))
-|> Frame.fillMissingWith 0
-|> Chart.Bar 
-|> Chart.WithLegend true 
-
-
-
-//
-
-//bug in newer XPLot.Deedle, could do , has to update some references
-// Workaround with convert frame to series
-// let rows : Series<string,Series<string,int>> = data |> Frame.getRows
-// rows |> Series.values |> Chart.Bar |> Chart.WithLabels rows.Keys
